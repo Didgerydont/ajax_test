30be89d21db0c854cab5d1510ba6d5ee39c92fd9
Ubuntu <ubuntu@ip-172-31-83-149.ec2.internal>
2019-06-19 14:58:56 +0000

    Working With External Resources   Data & The DOM  Unpacking Our Data Onto The DOM

---
+++ main.js
@@ -0,0 +1,25 @@
+const baseURL = "https://swapi.co/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
